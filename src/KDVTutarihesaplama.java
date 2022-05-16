581d5cb147cb889aa646d57f676696a5cd3a44c9
Ibrahim Can Gunaydin <ibrahimcangunaydin@gmail.com>
2022-05-16 19:47:12 +0300

    KDVTutarihesaplama added

---
+++ src/KDVTutarihesaplama.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class KDVTutarihesaplama {
+    public static void main(String[] args) {
+        double tutar, kdvOran=0.18, kdvTutar,kdvliTutar;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Ucret Tutarini Giriniz: ");
+        tutar = inp.nextDouble();
+
+        kdvTutar=tutar*kdvOran;
+        kdvliTutar=tutar+kdvTutar;
+
+        System.out.println("KDV'siz Tutar: "+tutar);
+        System.out.println("KDV Orani: "+kdvOran);
+        System.out.println("KDV Tutari: "+kdvTutar);
+        System.out.println("KDV'li Tutar: "+kdvliTutar);
+
+    }
+
+}
